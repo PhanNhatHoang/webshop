3da251d9f2100ca9a013b9cad934b89d531e39bf
hoang <phanhoang25112001@gmail.com>
2024-01-08 19:18:09 +0700

    RoleandUser

---
+++ src/main/java/model/Role.java
@@ -0,0 +1,21 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name= "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_ID")
+    private Long id;
+
+    private String name;
+
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,20 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_ID")
+    private Long id;
+
+
+    private String fullname;
+
+}
